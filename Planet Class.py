356ea9497af7e0c376af6f9812a83d977e931acc
Sam Edgar <LuckySalmon@users.noreply.github.com>
2020-05-02 13:10:00 -0600

    Update Planet Class.py

--- Planet Class.py
+++ Planet Class.py
@@ -9,10 +9,9 @@ g=6.674 * (10 ** -11)
 # Velocity will later be changed to only allow stable orbits.
 # Habitable will later be changed to depend on the star's habitable zone.
 
-class Planet():
+class Planet:
     def __init__(self):
         self.name = "p" + str(random.randint(1, 15))
-        print (self.name)
         self.distanceFromStar = float(random.random()*10) # measured in AU
         self.velocity = float(random.random()*10) # measured in years
         self.mass = float(random.random()*10) # measured in Earths
@@ -23,13 +22,16 @@ class Planet():
         self.gravity = "placeholder" # input the gravity calculation
 
 
-class Star():
+class Star:
     def __init__(self):
         self.name = "star"
         self.radius = float(random.randint(10000, 700000000)) # measured in km
         self.numberOfPlanets = random.randint(3, 6)
+        self.planetList = []
         for i in range(self.numberOfPlanets):
-            Planet()
+            self.planetList.append(Planet())
+
+        #print (self.planetList)
         self.temperature = random.randint(2000, 27000) # measured in kelvin
         if self.temperature <= 3500:
             self.color = "Red"
